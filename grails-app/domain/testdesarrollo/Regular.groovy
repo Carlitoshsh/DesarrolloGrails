776a26072561151e81e08c28a76b4dcdb9ffeab6
mppinedav <mppinedav@unal.edu.co>
2015-10-21 23:42:40 -0500

    Reestablecido Regular

---
+++ grails-app/domain/testdesarrollo/Regular.groovy
@@ -0,0 +1,17 @@
+package testdesarrollo
+
+
+
+class Regular extends User{
+
+    int postViews = 0
+    int strikesNumber
+    int starsNumber
+
+    static constraints = {
+        postViews min: 0, nullable: false
+        strikesNumber range: 0..3, nullable: false
+        starsNumber range: 0..5, nullable: false
+    }
+
+}
